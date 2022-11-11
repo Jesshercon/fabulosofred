c850512dcfb46e627ce0497de95c3379cda1f5b2
yosassenav <mromerovanessa.soy@gmail.com>
2022-11-11 01:22:42 -0500

    first attempt

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const sequence = [];
+const userSequence = [];
+const buttons = Array.from(document.getElementsByTagName("button"));
 
+const getRandomArbitraty = (max, min) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const reproduceSequence = () => {
+  buttons.forEach((button) => {
+    const randomNum = getRandomArbitraty(25, 1);
+    console.log(buttons.getElementById(button));
+
+    console.log(randomNum);
+  });
+};
+
+reproduceSequence();
